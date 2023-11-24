4ca2c260b7892bb2e4a021da9804f5d6a12fc967
Elad Avneri <eladavn@gmail.com>
2023-11-24 13:01:11 +0200

    "findings" skeleton

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FindingsModule } from './findings/findings.module';
 
 @Module({
-  imports: [],
+  imports: [FindingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/findings/findings.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('findings')
+export class FindingsController {}
