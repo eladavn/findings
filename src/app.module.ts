a908ed3c4d09b55127fbeb85f0bb89f6c15fdef7
Elad Avneri <eladavn@gmail.com>
2023-11-28 18:28:17 +0200

    Main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FindingsModule } from './findings/findings.module';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
 
 @Module({
-  imports: [FindingsModule],
+  imports: [
+    FindingsModule,
+    TypeOrmModule.forRoot({
+      type: "sqljs",
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoSave: true,
+      location: 'findings.sqlite'
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
