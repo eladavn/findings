06ffd5f112cbefb80964b9e14c280d3d4b3cbbeb
Elad Avneri <eladavn@gmail.com>
2023-11-29 19:48:06 +0200

    Refactoring

--- src/tenantDBs/registeredTenants.module.ts
+++ src/tenantDBs/registeredTenants.module.ts
@@ -7,9 +7,65 @@ import {Finding} from '../findings/finding.entity';
 import {Resource} from '../findings/resource.entity';
 
 import { RegisteredTenant } from './registeredTenant.entity';
-import { assert } from 'console';
 
 export const TENANT_DATA_SOURCE = 'TENANT_DATA_SOURCE';
+
+async function registerNewTenant(tenantsRepo, requestTenantId: number) {
+  const registeredTenantsCount = await tenantsRepo.count();
+
+  // Since the list of registered tenants is append only, a newly introduced tenant must get the last used
+  // DB or a newly created one.
+  const dbIndex = Math.floor(registeredTenantsCount / 3) + 1;
+  const newRegisteredTenant = tenantsRepo.create({
+    tenantId: requestTenantId,
+    dbIndex: dbIndex
+  });
+
+  tenantsRepo.save(newRegisteredTenant);
+  return dbIndex;
+}
+
+async function getDbIndex(request : Request, registeredTenantsDataSource: DataSource) : Promise<number> {
+  const tenantsRepo = registeredTenantsDataSource.getRepository(RegisteredTenant);
+  const requestTenantId = parseInt(request.params.tenantId);
+  const registeredTenant = await tenantsRepo.findOne(({ where: { tenantId: requestTenantId } }));
+  if (registeredTenant) {
+    // Tenant already registered
+    return registeredTenant.dbIndex;
+  }
+
+  // Tenant was never registered
+
+  if (request.method != 'POST') {
+    // The given tenant was never registered and it shouldn't be registered here as well 
+    // (as registration is relevant only for POST request)
+    // So the given tenant is not to be found in any of the DBs, so just work
+    // with arbitrary DB which would result in standard entity not available behavior
+    return 1;
+  }
+
+  return registerNewTenant(tenantsRepo, requestTenantId);
+}
+
+
+async function createTenantDataSource(request : Request, registeredTenantsDataSource : DataSource) : Promise<DataSource> {
+  
+  let dbIndex: number = await getDbIndex(request, registeredTenantsDataSource);
+
+  const dbName = `findings${dbIndex}.sqlite`;
+  console.log('Using ' + dbName);
+
+  const tenantDataSource = new DataSource({
+    type: "sqljs",
+    entities: [Finding,Resource],
+    autoSave: true,
+    location: dbName,
+    synchronize: true
+  });
+
+  return tenantDataSource.initialize();
+}
+
 
 @Module({
   imports: [
@@ -29,45 +85,7 @@ export const TENANT_DATA_SOURCE = 'TENANT_DATA_SOURCE';
         DataSource,
       ],
       scope: Scope.REQUEST,
-      useFactory: async (request : Request, registeredTenantsDataSource : DataSource) => {
-        const tenantsRepo = registeredTenantsDataSource.getRepository(RegisteredTenant);
-        const requestTenantId = parseInt(request.params.tenantId);
-        const registeredTenant: RegisteredTenant = await tenantsRepo.findOne(({ where: { tenantId: requestTenantId } }));
-        let dbIndex :number; 
-        if (!registeredTenant) {
-
-          if (request.method === 'POST') {
-            const registeredTenantsCount = await tenantsRepo.count();
-
-            dbIndex = Math.floor(registeredTenantsCount /3) +1;
-            const newRegisteredTenant = tenantsRepo.create({
-              tenantId: requestTenantId,
-              dbIndex: dbIndex
-            });
-  
-            tenantsRepo.save(newRegisteredTenant);
-          }
-          else {
-            dbIndex = 1;
-          }
-
-        }
-        else {
-          dbIndex = registeredTenant.dbIndex;
-        }
-
-        const dbName = `findings${dbIndex}.sqlite`;
-        console.log('Using ' + dbName);
-
-        const tenantDataSource = new DataSource({
-          type: "sqljs",
-          entities: [Finding,Resource],
-          autoSave: true,
-          location: dbName,
-          synchronize: true
-        });
-        return await tenantDataSource.initialize();
-      }
+      useFactory: createTenantDataSource
     }
   ],
   exports: [
@@ -75,4 +93,7 @@ export const TENANT_DATA_SOURCE = 'TENANT_DATA_SOURCE';
   ]
 })
 export class RegisteredTenantsModule {};
+
+
+
 
